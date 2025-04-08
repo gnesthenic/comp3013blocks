cbb2b3545b04b1d2836bd56f0334ddf6df446e9b
gnesthenic <nicolaspenner@shaw.ca>
2025-04-07 23:15:43 -0700

    Fix handleEditorChange function in CodeEditor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -4,10 +4,14 @@ import { useState } from "react";
 
 export function CodeEditor({ existingCode }: any) {
   const [code, setCode] = useState("");
-  // function handleEditorChange(value: string | undefined) {
-  //   // Debounce Function
-  //   debounce(() => saveToDatabase(code))
-  // }
+
+  function handleEditorChange(value: string | undefined) {
+    if (value !== undefined) {
+      setCode(value); 
+      console.log("Editor content changed:", value);
+    }
+  }
+
   return (
     <Editor
       height="90vh"
